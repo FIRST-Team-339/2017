f21e4edc2476145a666f77661e8ac0fd32b36003
Becky Button <einsteinunicorn@gmail.com>
2017-02-08 19:43:05 -0500

    Kilroy servo

--- src/org/usfirst/frc/team339/HardwareInterfaces/KilroyServo.java
+++ src/org/usfirst/frc/team339/HardwareInterfaces/KilroyServo.java
@@ -6,8 +6,13 @@ public class KilroyServo // extends Servo
 {
 
 /**
+ * Kilroy Servo
+ * 
  * @param portNumber
+ *            the port the servo is on
  * @param maxDegrees
+ *            the maximum degrees the servo can go
+ * @author Becky Button
  */
 public KilroyServo (int portNumber, double maxDegrees)
 {
@@ -18,12 +23,23 @@ public KilroyServo (int portNumber, double maxDegrees)
 
 private double maxDegrees = 0;
 
+/**
+ * Scales raw set value to degrees
+ * 
+ * @param degree
+ *            you want to set the servo
+ */
 public void setAngle (double degree)
 {
     double setVal = degree / this.maxDegrees; // scaled value for servo
     this.servo.set(setVal);
 }
 
+/**
+ * scales raw value to degrees
+ * 
+ * @return Angle value in degrees
+ */
 public double getAngle ()
 {
     double angleValue = this.servo.get() * this.maxDegrees;
