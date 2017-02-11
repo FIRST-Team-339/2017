741b94657093d14a5ddc9c7e11e86725670e8170
Becky Button <einsteinunicorn@gmail.com>
2017-02-11 11:31:19 -0500

    Finished Gyro

--- src/org/usfirst/frc/team339/HardwareInterfaces/KilroyGyro.java
+++ src/org/usfirst/frc/team339/HardwareInterfaces/KilroyGyro.java
@@ -1,54 +1,63 @@
 package org.usfirst.frc.team339.HardwareInterfaces;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.SPI;
-
 
 public class KilroyGyro
 {
 private final ADXRS450_Gyro gyro;
-private SPI m_spi;
+
+private boolean hasGyro = true;
 
 public KilroyGyro (boolean hasGyro)
 {
     if (hasGyro == false)
         {
         System.out.println("Gyro not connected");
+        this.hasGyro = false;
         }
     if (hasGyro)
         {
         this.gyro = new ADXRS450_Gyro();
+        this.hasGyro = true;
         }
     else
         this.gyro = null;
 }
 
-public void calibrateToZero ()
+public void calibrate ()
 {
-    if (m_spi == null)
+    if (hasGyro == false)
         {
         return;
         }
-    m_spi.resetAccumulator();
-    m_spi.setAccumulatorCenter(0);
+    this.calibrate();
 }
 
 public void reset ()
 {
-    if (m_spi == null)
+    if (hasGyro == false)
         {
         return;
         }
-    m_spi.resetAccumulator();
+    this.reset();
 }
 
 public double getAngle ()
 {
-    if (m_spi == null)
+    if (hasGyro == false)
         {
-        return 888888;
+        return 339339;
         }
-    return m_spi.getAccumulatorValue();
+    return this.getAngle();
 
+}
+
+public double getRate ()
+{
+    if (hasGyro == false)
+        {
+        return 339339;
+        }
+    return this.getRate();
 }
 }
