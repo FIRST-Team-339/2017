b75327ca1c449b8f6b7c14683664220fb8e14544
Ryan McGee <ramtech51@comcast.net>
2017-02-16 21:07:55 -0500

    Need to fix the error in hardware...

--- src/org/usfirst/frc/team339/robot/Teleop.java
+++ src/org/usfirst/frc/team339/robot/Teleop.java
@@ -33,6 +33,9 @@ package org.usfirst.frc.team339.robot;
 
 import org.usfirst.frc.team339.Hardware.Hardware;
 import org.usfirst.frc.team339.Utils.Drive;
+
+import com.ctre.CANTalon.FeedbackDevice;
+
 import edu.wpi.first.wpilibj.Relay;
 
 /**
@@ -133,6 +136,8 @@ public class Teleop {
 			// Hardware.mecanumDrive.setDirectionalDeadzone(0.2);
 
 		}
+		
+		Hardware.gimbalMotor.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Absolute);
 
 		isAligning = false;
 		isStrafingToTarget = false;
@@ -183,13 +188,7 @@ public class Teleop {
 		printStatements();
 
 		// TESTING CODE:
-
-		if (Hardware.rightOperator.getRawButton(2))
-			Hardware.intake.startIntake();
-		else if (Hardware.rightOperator.getRawButton(3))
-			Hardware.intake.reverseIntake();
-		else
-			Hardware.intake.stopIntake();
+		
 
 		// =================================================================
 		// Driving code
@@ -209,6 +208,20 @@ public class Teleop {
 			else
 				Hardware.tankDrive.drive(Hardware.rightDriver.getY(), Hardware.leftDriver.getY());
 		}
+		
+		// =================================================================
+		// OPERATOR CONTROLS
+		// =================================================================
+		
+		if(Hardware.leftOperator.getRawButton(2))
+			Hardware.shooter.turnGimbalSlow(Hardware.leftOperator.getX() > 0 ? 1 : -1);
+		
+		if (Hardware.rightOperator.getRawButton(2))
+			Hardware.intake.startIntake();
+		else if (Hardware.rightOperator.getRawButton(3))
+			Hardware.intake.reverseIntake();
+		else
+			Hardware.intake.stopIntake();
 
 		// =================================================================
 		// CAMERA CODE
@@ -304,6 +317,8 @@ public class Teleop {
 		// prints value of the CAN controllers
 		// =================================
 		// Hardware.CAN.printAllPDPChannels();
+		
+		System.out.println("Gimbal Encoder: " + Hardware.gimbalMotor.getAnalogInPosition());
 
 		// =================================
 		// Relay
