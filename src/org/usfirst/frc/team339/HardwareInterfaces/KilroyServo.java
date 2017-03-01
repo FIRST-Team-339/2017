b7ab4987d7d2f3fb981024cc1ec5a2c5587e22b1
Ashley Espeland <catgirl2359@gmail.com>
2017-02-28 19:10:03 -0500

    commented the KilroyServo class

--- src/org/usfirst/frc/team339/HardwareInterfaces/KilroyServo.java
+++ src/org/usfirst/frc/team339/HardwareInterfaces/KilroyServo.java
@@ -16,10 +16,13 @@ public class KilroyServo extends Servo
  */
 public KilroyServo (int portNumber, double maxDegrees)
 {
+    // sets the port number to the one set in KilroyServo
     super(portNumber);
+    // sets up shorthand where this.maxDegrees is the same as maxDegrees
     this.maxDegrees = maxDegrees;
 }
 
+// sets maxDegrees equal to 0
 private double maxDegrees = 0; // max degrees servo can go
 
 /**
@@ -28,9 +31,12 @@ private double maxDegrees = 0; // max degrees servo can go
  * @param degree
  *            you want to set the servo
  */
+// sets angle of the servo
 public void setAngle (double degree)
 {
+    // divides the setAngle value by the maxDegrees to scale it from 0 to 1
     double setVal = degree / this.maxDegrees; // scaled value for servo
+    // sets the angle using the set() from KilroyServo
     super.set(setVal);
 }
 
@@ -39,10 +45,15 @@ public void setAngle (double degree)
  * 
  * @return Angle value in degrees
  */
+//
 public double getAngle ()
 {
+    // gets angle value thats already scaled from 0-1, and then scales it to
+    // degrees
+    // by multiplying it by the maxDegrees
     double angleValue = super.get() * this.maxDegrees; // scaled value for
                                                        // servo
+    // returns angle value
     return angleValue;
 
 }
